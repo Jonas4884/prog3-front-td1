c3c9bde251781332d5e63ec004605e459294327b
Jonas4884 <hei.jonatana@gmail.com>
2023-02-03 10:03:09 +0300

    chore : add form test about typing and input mode

--- src/__test__/form.test.js
+++ src/__test__/form.test.js
@@ -15,5 +15,28 @@ describe('should return the todo add task', () => {
         const toHaveaLabel = screen.getByLabelText("Press enter to add Task");
         expect(toHaveaLabel).toBeInTheDocument();
       })
+
+      test('should return a value after focusing on input',  () => {
+          const addTodo = "jadd okoko"
+
+        render(<TodoForm add={[addTodo]}/>)
+        const thePlaceholder = screen.getByPlaceholderText("Add new task")
+        expect(thePlaceholder).toBeInTheDocument();
+        expect(addTodo).toBeInTheDocument();
+      })
+
+      
+      test('should disapear the input on enter typing',  () => {
+        const addTodo = "jadd okoko"
+      render(<TodoForm add={[addTodo]}/>)
+      const thePlaceholder = screen.getByPlaceholderText("Add new task")
+      thePlaceholder.onmouseenter()
+      expect(thePlaceholder).toBeInTheDocument();
+     expect(addTodo).not.toBeInTheDocument();
+    })
+
+    
+      
+
       
  })
